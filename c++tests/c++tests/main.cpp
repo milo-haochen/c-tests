4459a750bdf6ccb6fdb8ad8aea4f0af38a2ed917
Haochen <meethaochen@163.com>
2020-06-12 16:10:07 +0800

    First Commit

---
+++ c++tests/c++tests/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  c++tests
+//
+//  Created by 浩宸 on 2020/6/12.
+//  Copyright © 2020 浩宸. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
